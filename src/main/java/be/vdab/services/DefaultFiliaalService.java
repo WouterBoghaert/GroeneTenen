0dd5ba3cef02d41066000449c1c52a3b6b38a669
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2018-01-17 20:08:27 +0100

    Hoofdstuk 31

--- src/main/java/be/vdab/services/DefaultFiliaalService.java
+++ src/main/java/be/vdab/services/DefaultFiliaalService.java
@@ -1,5 +1,6 @@
 package be.vdab.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,5 +65,16 @@ class DefaultFiliaalService implements FiliaalService {
 			.findByAdresPostcodeBetweenOrderByNaam(
 				reeks.getVanpostcode(),
 				reeks.getTotpostcode());
+	}
+	
+	@Override
+	public List<Filiaal> findNietAfgeschreven() {
+		return filiaalRepository.findByWaardeGebouwNot(BigDecimal.ZERO);
+	}
+	
+	@Override
+	@ModifyingTransactionalServiceMethod
+	public void afschrijven(List<Filiaal> filialen) {
+		filialen.forEach(filiaal -> filiaal.afschrijven());
 	}
 }
