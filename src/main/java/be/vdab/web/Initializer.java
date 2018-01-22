c6fcce53496838c14050b28ab6a9199cbc2b59a9
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2018-01-22 16:26:28 +0100

    Hoofdstuk 36

--- src/main/java/be/vdab/web/Initializer.java
+++ src/main/java/be/vdab/web/Initializer.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import be.vdab.datasource.DataSourceConfig;
 import be.vdab.repositories.RepositoriesConfig;
+import be.vdab.restclients.RestClientsConfig;
 import be.vdab.restservices.RestControllersConfig;
 import be.vdab.services.ServicesConfig;
 
@@ -21,7 +22,7 @@ public class Initializer
 	@Override
 	protected Class<?> [] getRootConfigClasses() {
 		return new Class<?>[] {DataSourceConfig.class, RepositoriesConfig.class, 
-			ServicesConfig.class }; 
+			ServicesConfig.class, RestClientsConfig.class }; 
 	}
 	
 	@Override
