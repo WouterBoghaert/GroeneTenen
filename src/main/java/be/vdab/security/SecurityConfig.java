f9108be6c21b55c84f58b3b280cb662e251725e9
cursist <cursist@LOK111-05>
2018-01-23 13:43:03 +0100

    Hoofdstuk 42

--- src/main/java/be/vdab/security/SecurityConfig.java
+++ src/main/java/be/vdab/security/SecurityConfig.java
@@ -1,26 +1,47 @@
 package be.vdab.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String MANAGER = "manager";
 	private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
 	private static final String MAGAZIJNIER = "magazijnier";
+	private final DataSource dataSource;
+	private static final String USER_BY_USERNAME =
+		"select naam as usernaam, paswoord as password, actief as enabled " +
+		"from gebruikers where naam = ?";
+	private static final String AUTHORITIES_BY_USERNAME =
+		"select gebruikers.naam as username, rollen.naam as authorities" +
+		" from gebruikers inner join gebruikersrollen" +
+		" on gebruikers.id = gebruikersrollen.gebruikerid" +
+		" inner join rollen" +
+		" on rollen.id = gebruikersrollen.rolid" +
+		" where gebruikers.naam = ?";
+		
+		
+	public SecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("joe").password("theboss").authorities(MANAGER)
-			.and().withUser("averell").password("hungry")
-			.authorities(HELPDESKMEDEWERKER, MAGAZIJNIER);
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery(USER_BY_USERNAME)
+			.authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME)
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	@Override
@@ -35,13 +56,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().loginPage("/login").and().logout().logoutSuccessUrl("/")
 			.and().authorizeRequests()
-			.mvcMatchers("/filialen/toevoegen", "filialen/*/wijzigen",
+			.mvcMatchers("/filialen/toevoegen", "/filialen/*/wijzigen",
 				"/filialen/*/verwijderen").hasAuthority(MANAGER)
 			.mvcMatchers(HttpMethod.POST, "/filialen").hasAuthority(MANAGER)
 			.mvcMatchers("/werknemers")
 			.hasAnyAuthority(MAGAZIJNIER, HELPDESKMEDEWERKER)
+			.mvcMatchers(HttpMethod.PUT, "filialen/*").hasAuthority(MANAGER)
+			.mvcMatchers(HttpMethod.DELETE, "/filialen/*").hasAuthority(MANAGER)
 			.mvcMatchers("/", "/login").permitAll()
 			.mvcMatchers("/**").authenticated()
 			.and().exceptionHandling().accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
+		http.httpBasic();
 	}
 }
