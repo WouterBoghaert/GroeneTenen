2195e7983399c955c35370d2a8e463c96b3d857c
cursist <cursist@LOK111-05>
2018-01-23 15:44:23 +0100

    Hoofdstuk 43.6.1

--- src/main/java/be/vdab/web/Initializer.java
+++ src/main/java/be/vdab/web/Initializer.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import be.vdab.aop.AOPConfig;
 import be.vdab.datasource.DataSourceConfig;
 import be.vdab.jms.JmsConfig;
 import be.vdab.mail.MailConfig;
@@ -25,7 +26,7 @@ public class Initializer
 	protected Class<?> [] getRootConfigClasses() {
 		return new Class<?>[] {DataSourceConfig.class, RepositoriesConfig.class, 
 			ServicesConfig.class, RestClientsConfig.class, MailConfig.class,
-			JmsConfig.class, SecurityConfig.class}; 
+			JmsConfig.class, SecurityConfig.class, AOPConfig.class}; 
 	}
 	
 	@Override
