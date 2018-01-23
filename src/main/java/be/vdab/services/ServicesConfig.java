cd0804e49aa6f80a117b24575bf42b6dbac41a95
cursist <cursist@LOK111-05>
2018-01-23 09:30:13 +0100

    Hoofdstuk 40

--- src/main/java/be/vdab/services/ServicesConfig.java
+++ src/main/java/be/vdab/services/ServicesConfig.java
@@ -2,8 +2,10 @@ package be.vdab.services;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
