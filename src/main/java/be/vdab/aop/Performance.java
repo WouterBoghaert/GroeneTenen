f137a9a9cd8274ad3150702caae196ba6ddc7007
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2018-01-25 15:26:07 +0100

    Hoofdstuk 43

--- src/main/java/be/vdab/aop/Performance.java
+++ src/main/java/be/vdab/aop/Performance.java
@@ -6,10 +6,12 @@ import java.util.logging.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 class Performance {
 	private static final Logger LOGGER = Logger.getLogger(Performance.class.getName());
 	@Around("be.vdab.aop.PointcutExpressions.services()")
